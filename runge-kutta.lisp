1ae89cd91ccac8370f252e6181a909d076d6960f
Stefan Gille <st_gille@web.de>
2012-12-04 02:02:05 +0100

    clean up: runge-kutta

--- runge-kutta.lisp
+++ runge-kutta.lisp
@@ -4,13 +4,13 @@
 
 (defun central-diff-quot (f)
   "Return new function approximating the derivative of input."
-  #'(lambda (x)
-      (let* ((x+ (+ x *eps*))
-             (x- (- x *eps*))
-             (fx+ (funcall f x+))
-             (fx- (funcall f x-))
-             (formula (lambda (fx fy) (* 0.5 (/ 1.0 *eps*) (- fx fy)))))
-        (mapcar formula fx+ fx-))))
+  (lambda (x)
+    (let* ((x+ (+ x *eps*))
+           (x- (- x *eps*))
+           (fx+ (funcall f x+))
+           (fx- (funcall f x-))
+           (formula (lambda (fx fy) (* 0.5 (/ 1.0 *eps*) (- fx fy)))))
+      (mapcar formula fx+ fx-))))
 
 (defmacro make-autonomous (f)
   (let ((x (gensym)))
@@ -55,30 +55,12 @@
       (list ft0)
       (cons ft0 (eval-f f (+ t0 stepsize) t1 stepsize)))))
 
-(untrace runge-kutta runge-kutta-single-step solve-rks)
-
 (defun f1 (x y)
-  (list (first y) (* x x)))
-(let* ((x0 '(1.0 1.0))
+  (list (first y) (third y) (fourth y) (second y) (* x x)))
+
+(let* ((x0 '(1.0 1.0 1.0 1.0 1.0))
        (t0 0.0)
-       (inc 0.01)
+       (inc 0.001)
        (t1 1))
-  (progn (pprint (runge-kutta 'f1 x0 t0 t1 inc))
-         (pprint (eval-f (lambda (x) (list (* (first x0) (exp x)) (+ (cadr x0) (* 1/3 x x x)))) t0 t1 inc))
-         t))
-(runge-kutta-single-step 'f1 '(1.0 1.0) 0.0 0.01)
-
-(defun f2 (tt x)
-  x)
-
-(defun f3 (tt x)
-  '(1))
-
-(progn (runge-kutta-single-step 'f2 '(1.0) 0.0 0.01)
-       (eval-f (lambda (x) (exp x)) 0.0 0.01 0.01))
-
-(runge-kutta 'f3 '(1.0) 0.0 0.2 0.01)
-(runge-kutta-single-step 'f3 '(1.0) 0.0 0.01)
-(eval-f (lambda (x) (+ 1 x)) 0.0 0.2 0.01)
-
-
+  (time (runge-kutta 'f1 x0 t0 t1 inc))
+  t)
