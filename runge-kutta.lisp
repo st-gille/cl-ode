429130165a3b212b1617ba0c797ea334b19737c4
Stefan Gille <st_gille@web.de>
2012-12-10 00:49:55 +0100

    style

--- runge-kutta.lisp
+++ runge-kutta.lisp
@@ -1,10 +1,11 @@
 (in-package :ode)
 
 (defstruct (butcher-tableau
-             (:constructor make-butcher (&optional (matrix '((0)))
-                                                   (weights '(1))
-                                                   (time-coeffs '(0))
-                                                   (is-implicit (not (lower-triangular-p matrix))))))
+             (:constructor make-butcher
+                           (&optional (matrix '((0)))
+                                      (weights '(1))
+                                      (time-coeffs '(0))
+                                      (is-implicit (not (lower-triangular-p matrix))))))
   (matrix :read-only)
   (weights :read-only)
   (time-coeffs :read-only)
@@ -44,7 +45,9 @@
   Pass either an object of type butcher-tableau or a symbol.
   If passed a symbol, it's associated tableau will
   be retrieved from a pre-defined list."
-  (let1 (m (if (symbolp tableau) `(cdr (assoc (quote ,tableau) *tableaus*)) tableau))
+  (let1 (m (if (symbolp tableau)
+             `(cdr (assoc (quote ,tableau) *tableaus*))
+             tableau))
     `(let1 (*selected-tableau* ,m)
        (if (any-p *selected-tableau* null (not butcher-tableau-p))
          (error "Not a valid butcher-tableau: ~A~%" *selected-tableau*))
@@ -58,17 +61,20 @@
       (apply #'mapcar (list #'fma x (swap-matrix-layout k))))
     x))
 
-(defun scaled-ode-eval (ode stepsize ai ci k t0 x0)
+(defun ode-eval (ode stepsize k x0 t0 ai ci)
+  (funcall ode
+           (+ t0 (* ci stepsize))
+           (eval-point x0 k ai)))
+
+(defun scaled-ode-eval (ode stepsize k x0 t0 ai ci)
   (mapcar (lambda (fi) (* stepsize fi))
-          (funcall ode
-                   (+ t0 (* ci stepsize))
-                   (eval-point x0 k ai))))
+          (ode-eval ode stepsize k x0 t0 ai ci)))
 
 (defun solve-explicit-rks (ode x0 t0 stepsize)
-  (let ((k))
+  (let1 (k)
     (loop for ai in *A*
           for ci in *c*
-          do (let ((ki (list (scaled-ode-eval ode stepsize ai ci k t0 x0))))
+          do (let1 (ki (list (scaled-ode-eval ode stepsize k x0 t0 ai ci)))
                (appendf k ki)))
     k))
 
@@ -76,21 +82,31 @@
   (let* ((dim (length x0))
          (rk-steps  (length *c*))
          (len (* dim rk-steps)))
-    (labels ((weight-diff (kij fj) (- kij (* fj stepsize)))
-             (eqs-for-ki (ki ode-system) (mapcar #'weight-diff ki ode-system))
-             (ode-system-for-step (k) (flet ((ode-eval (ai ci)
-                                                       (funcall ode
-                                                                (+ t0 (* ci stepsize))
-                                                                (eval-point x0 k ai))))
-                                        (mapcar #'ode-eval *A* *c*)))
-             (implicit-system (k-flat) (let ((k (split k-flat dim)))
-                                         (mapcan #'eqs-for-ki k (ode-system-for-step k)))))
+    (labels ((weight-diff (kij fj)
+               (- kij (* fj stepsize)))
+             (eqs-for-ki (ki ode-system)
+               (mapcar #'weight-diff
+                       ki
+                       ode-system))
+             (ode-system-for-step (k)
+               (mapcar (curry #'ode-eval ode stepsize k x0 t0)
+                       *A*
+                       *c*))
+             (implicit-system (k-flat)
+               (let1 (k (split k-flat dim))
+                 (mapcan #'eqs-for-ki
+                         k
+                         (ode-system-for-step k)))))
       (split (newton-solver #'implicit-system len) dim))))
 
 (defun runge-kutta-single-step (f x0 t0 stepsize)
   (let* ((solver (if *is-implicit* #'solve-implicit-rks #'solve-explicit-rks))
          (k (funcall solver f x0 t0 stepsize))
-         (bk (mapcar (lambda (bi ki) (mapcar (lambda (kij) (* bi kij)) ki)) *b* k)))
+         (bk (mapcar (lambda (bi ki)
+                       (mapcar (lambda (kij) (* bi kij))
+                               ki))
+                     *b*
+                     k)))
     (apply #'mapcar (cons #'+ (cons x0 bk)))))
 
 (defun ensure-last-step (ode last-x last-t t1)
@@ -137,12 +153,11 @@
         return i))
 
 (defun insert-at (place-to-put new-points)
-  (let ((new-tail (cdr place-to-put)))
-    (if new-tail
-      (setf (cdr place-to-put) (append new-points new-tail))
-      (nconc place-to-put new-points))))
+  (let1 (tail (cdr place-to-put))
+    (setf (cdr place-to-put) (append new-points tail))))
 
-(defun cached-ode-solution (the-ode init-t0 init-x0 init-stepsize)
+(defun cached-ode-solution (the-ode init-t0 init-x0 &optional (init-stepsize *step*))
+  "Create a closure caching already calculated values of the solution."
   (let ((stepsize (max *eps* init-stepsize))
         (known-graph (list (list init-t0 init-x0))))
     (values
@@ -152,11 +167,9 @@
                (x0 (cadar closest)))
           (if (< t1 t0)
             (error "Can't go back in time."))
-          (cond
-            ((num-equal t0 t1)
-             x0)
-            (t
-              (let ((graph (runge-kutta-graph the-ode x0 t0 t1 stepsize)))
-                (insert-at closest graph)
-                (cadar (last graph)))))))
+          (if (num-equal t0 t1)
+            x0
+            (let1 (graph (runge-kutta-graph the-ode x0 t0 t1 stepsize))
+              (insert-at closest graph)
+              (cadar (last graph))))))
       (lambda () known-graph))))
